bfc24770d95444459482dfd0896c9e51a3b17534
Charles White <ntrotman008@gmail.com>
2024-03-07 13:26:12 -0400

    crud Operations

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import express from "express";
+import express, { response } from "express";
 import { PORT, MongoDbURL } from "./config.js";
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
 const app = express()
 
 
@@ -28,17 +28,40 @@ app.get('/', (req, res)=> {
 })
 
 app.get('/shop', (req, res)=> {
-    res.status(202). send("<h1> Nobody Knows me like you do</h1>")
-})
+    
+    myBooks.find().toArray()
+    .then(response=>{
+        //console.log(response)
+        res.status(200).send(response)
 
+    })
+    .catch(error=> console.log(error))
+    //route show all times
+    //res.status(202). send("<h1> Nobody Knows me like you do</h1>")
+})
 
 
 
 app.get('/shop/:id', (req, res)=> {
+    // Route show a specific id
     const data = req.params
-    res.status(202).send(`<a href='/'> Book: ${data.id}</a`)
+
+    const filter = {
+        "_id": new ObjectId(data.id)
+        
+    }
+
+    myBooks.findOne(filter)
+    .then(response=>{
+        console.log(response)
+        res.status(200).send(response)
+
+    })
+    .catch(error=> console.log(error))
+    //res.status(202).send(`<a href='/'> Book: ${data.id}</a`)
 })
-app.post('/savebook',(req, res)=>{
+app.post('/admin/savebook',(req, res)=>{
+    // Route adds a new book
     const data = req.body
     if (!data.title)
     return res.status(400).send("No titloe found")
@@ -57,4 +80,41 @@ app.post('/savebook',(req, res)=>{
     })
   
     return res.status(201).send(JSON.stringify(data))
+})
+
+app.delete('/admin/remove/:id',(req, res)=>{
+    const data = req.params
+    const filter = {
+        "_id": new ObjectId(data.id)
+        
+    }
+
+    myBooks.deleteOne(filter)
+    .then(response=>{
+        console.log(response)
+        res.status(200).send(response)
+
+    })
+    .catch(error=> console.log(error))
+}) 
+
+app.put('/admin/update/:id',(req, res)=>{
+    const data = req.params
+    const docData = req.body
+    const filter = {
+        "_id": new ObjectId(data.id)
+    }
+
+    const updDoc = {
+        $set:{
+            //"price":data.price
+            ...docData
+        }
+    }
+
+    myBooks.updateOne(filter, updDoc)
+    .then(response=>{
+     res.status(200).send(response)
+    })
+    .catch(err=>console.log(err))
 })
